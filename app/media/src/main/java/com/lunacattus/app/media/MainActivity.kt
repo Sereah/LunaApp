632f574fe04ce3d1d0a6ab68659bfc652e43bfbc
Glacien <Glacien.Zhou@gmail.com>
2025-09-10 13:55:28 +0800

    add media service

--- app/media/src/main/java/com/lunacattus/app/media/MainActivity.kt
+++ app/media/src/main/java/com/lunacattus/app/media/MainActivity.kt
@@ -8,24 +8,14 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.sp
-import com.lunacattus.app.base.compose.components.BottomItem
 import com.lunacattus.app.media.theme.LunaAppTheme
-import com.lunacattus.speech.Speech
-import com.lunacattus.speech.wakeup.DUIWakeUp
 import dagger.hilt.android.AndroidEntryPoint
-import javax.inject.Inject
 
 @AndroidEntryPoint
 class MainActivity : ComponentActivity() {
-
-    @Inject lateinit var wakeup: DUIWakeUp
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -37,7 +27,7 @@ class MainActivity : ComponentActivity() {
                     contentAlignment = Alignment.Center
                 ) {
                     Button(onClick = {
-                        wakeup.start()
+
                     }) {
                         Text(text = "Start", fontSize = 20.sp)
                     }
---
+++ service/media/build.gradle.kts
@@ -0,0 +1,37 @@
+import java.text.SimpleDateFormat
+
+plugins {
+    alias(libs.plugins.architecture.android.application)
+    alias(libs.plugins.architecture.hilt)
+}
+
+android {
+    namespace = "com.lunacattus.service.media"
+
+    defaultConfig {
+        applicationId = "com.lunacattus.service.media"
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+
+    applicationVariants.configureEach {
+        val variant = this
+        val timestamp = SimpleDateFormat("yyyyMMddHHmmss").format(System.currentTimeMillis())
+        variant.outputs.configureEach {
+            (this as com.android.build.gradle.internal.api.BaseVariantOutputImpl).outputFileName =
+                "MediaService-${variant.name}-${variant.versionName}-${timestamp}.apk"
+        }
+    }
+
+    packaging {
+        resources.excludes += "META-INF/*.md"
+    }
+}
+
+dependencies {
+    implementation(project(":speech"))
+    implementation(project(":logger"))
+
+    debugImplementation(libs.leakcanary.android)
+}
