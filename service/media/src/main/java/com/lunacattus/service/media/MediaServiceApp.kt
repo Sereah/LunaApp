9c094149425c17effe628924ee5b8a32b81af430
Glacien <Glacien.Zhou@gmail.com>
2025-10-11 18:07:43 +0800

    commit

--- service/media/src/main/java/com/lunacattus/service/media/MediaServiceApp.kt
+++ service/media/src/main/java/com/lunacattus/service/media/MediaServiceApp.kt
@@ -4,21 +4,34 @@ import android.annotation.SuppressLint
 import android.app.Application
 import android.content.IntentFilter
 import com.lunacattus.logger.Logger
+import com.lunacattus.service.media.common.Contacts.ACTION_POWER_BUTTON_SHORT_LONG_PRESS
+import com.lunacattus.service.media.common.Contacts.TEST_ACTION_SPEECH_START_ASR
+import com.lunacattus.service.media.common.Contacts.TEST_ACTION_SPEECH_START_WAKEUP
 import com.lunacattus.service.media.common.TestReceiver
+import com.lunacattus.service.media.core.CoreReceiver
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
 class MediaServiceApp : Application() {
 
-    @SuppressLint("UnspecifiedRegisterReceiverFlag")
     override fun onCreate() {
         super.onCreate()
         Logger.initBaseTag(getString(R.string.app_name))
         Logger.d(TAG, "onCreate.")
-        val filter = IntentFilter().apply {
-            addAction("com.lunacattus.test")
+        initBroadcast()
+    }
+
+    @SuppressLint("UnspecifiedRegisterReceiverFlag")
+    private fun initBroadcast() {
+        val testFilter = IntentFilter().apply {
+            addAction(TEST_ACTION_SPEECH_START_WAKEUP)
+            addAction(TEST_ACTION_SPEECH_START_ASR)
         }
-        registerReceiver(TestReceiver(), filter)
+        val coreFilter = IntentFilter().apply {
+            addAction(ACTION_POWER_BUTTON_SHORT_LONG_PRESS)
+        }
+        registerReceiver(TestReceiver(), testFilter)
+        registerReceiver(CoreReceiver(), coreFilter)
     }
 
     companion object {
