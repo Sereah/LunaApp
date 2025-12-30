723dddf3020c21bab2d892aebc7ef100ad4d21b4
Glacien <Glacien.Zhou@gmail.com>
2025-12-30 20:30:50 +0800

    [conflux]切换黑白夜时切换状态栏主题

--- app/conflux/src/main/java/com/lunacattus/conflux/ui/MainActivity.kt
+++ app/conflux/src/main/java/com/lunacattus/conflux/ui/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.lunacattus.conflux.ui
 
 import android.Manifest
+import android.app.Activity
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.SystemBarStyle
@@ -16,13 +17,17 @@ import androidx.compose.material.icons.rounded.MusicVideo
 import androidx.compose.material.icons.rounded.Settings
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalView
+import androidx.core.view.WindowInsetsControllerCompat
 import androidx.lifecycle.viewmodel.navigation3.rememberViewModelStoreNavEntryDecorator
 import androidx.navigation3.runtime.entryProvider
 import androidx.navigation3.runtime.rememberDecoratedNavEntries
@@ -69,6 +74,7 @@ class MainActivity : ComponentActivity() {
                     dynamicColor = viewModel.dynamicColor,
                     darkTheme = viewModel.nightMode
                 ) {
+                    SystemBarAppearance(viewModel.nightMode)
                     val rootBackStack = rememberNavBackStack(MainGraph)
                     val innerNavigationState = rememberNavigationState(
                         startRoute = HomeRoute,
@@ -101,6 +107,18 @@ class MainActivity : ComponentActivity() {
             }
             ToastView()
         }
+    }
+}
+
+@Composable
+fun SystemBarAppearance(darkTheme: Boolean) {
+    val view = LocalView.current
+    val window = (view.context as Activity).window
+
+    SideEffect {
+        val controller = WindowInsetsControllerCompat(window, view)
+        controller.isAppearanceLightStatusBars = !darkTheme
+        controller.isAppearanceLightNavigationBars = !darkTheme
     }
 }
 
