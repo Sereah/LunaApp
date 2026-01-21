44f779a30c48b667dc3444347ef685d2555c429d
Glacien <glacien.zhou@gmail.com>
2026-01-21 11:28:43 +0800

    [conflux]添加androidauto跳转

--- app/conflux/src/main/java/com/lunacattus/conflux/ui/sections/connection/homepage/ConnectionRoute.kt
+++ app/conflux/src/main/java/com/lunacattus/conflux/ui/sections/connection/homepage/ConnectionRoute.kt
@@ -1,23 +1,64 @@
 package com.lunacattus.conflux.ui.sections.connection.homepage
 
-import androidx.compose.foundation.layout.Box
+import android.content.Intent
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.Text
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.DirectionsCar
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.lunacattus.conflux.ui.ActivityToastEvent
+import com.lunacattus.conflux.ui.LocalInnerPadding
+import com.lunacattus.conflux.ui.ToastEvent
+import com.lunacattus.conflux.ui.base.ItemCard
+import com.lunacattus.conflux.ui.base.NavigationItem
+import com.lunacattus.ui_design.compose.overScrollVertical
+import kotlinx.coroutines.launch
 
 @Composable
 fun ConnectionRoute(model: ConnectionViewModel) {
-    ConnectionScreen()
+    val context = LocalContext.current
+    val scope = rememberCoroutineScope()
+    ConnectionScreen(
+        navToAndroidAuto = {
+            Intent().apply {
+                action = "com.google.android.projection.gearhead.SETTINGS"
+            }.let {
+                runCatching {
+                    context.startActivity(it)
+                }.onFailure {
+                    scope.launch { ActivityToastEvent.send(ToastEvent.ShowToast("打开失败")) }
+                }
+            }
+        }
+    )
 }
 
 @Composable
-fun ConnectionScreen() {
-    Box(
-        modifier = Modifier.fillMaxSize(),
-        contentAlignment = Alignment.Center
+fun ConnectionScreen(
+    navToAndroidAuto: () -> Unit
+) {
+    val connectionItems = listOf(
+        NavigationItem(
+            title = "AndroidAuto",
+            icon = Icons.Rounded.DirectionsCar,
+            onClick = navToAndroidAuto
+        ),
+    )
+    LazyColumn(
+        modifier = Modifier
+            .fillMaxSize()
+            .overScrollVertical(),
+        contentPadding = LocalInnerPadding.current,
+        verticalArrangement = Arrangement.spacedBy(15.dp)
     ) {
-        Text("连接")
+
+        item {
+            ItemCard(connectionItems)
+        }
     }
 }
